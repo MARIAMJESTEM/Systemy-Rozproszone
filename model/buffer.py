c9c8299aebd162473776b1155eccf70b29f35e9f
agnieszkakojs <agakojs@student.agh.edu.pl>
2023-04-16 17:04:42 +0200

    addToBuffer function added

--- model/buffer.py
+++ model/buffer.py
@@ -1,10 +1,12 @@
 import numpy as np
-from .satelite import Satelite
+from satelite import Satellite
 
 class Buffer:
-    def __init__(self) -> None:
-        self.matrix = np.array([])
-        self.satelite: Satelite
+    def __init__(self, img_shape, satellite_name: Satellite) -> None:
+        self.matrix: np.ndarray = np.zeros(img_shape) #np.array([])
+        self.satelite: Satellite = satellite_name
 
-    def addToBuffer(self,posX: int, posY: int, posZ: int, val: int):
-        ...
+    def addToBuffer(self, posX: int, posY: int, posZ: int, val: int):
+        self.matrix[posZ][posX][posY] = val
+        # Jeżeli będzie indeksowanie od 1
+        # self.matrix[posZ-1][posX-1][posY-1] = val
