77400c631556591b25dcea0e15739447b4f1f242
agnieszkakojs <agakojs@student.agh.edu.pl>
2023-04-16 17:04:01 +0200

    add and delete satellite added

--- model/station.py
+++ model/station.py
@@ -1,14 +1,18 @@
 from typing import List
-from .satelite import Satelite
-from .buffer import Buffer
+from satelite import Satellite
+from buffer import Buffer
 
 class Station:
     def __init__(self) -> None:
-        self.satelitesList: List[Satelite]
+        self.satelitesList: List[Satellite] = []
         self.buffortList: List[Buffer]
 
-    def addSatelite(self):
-        ...
-    def deleteSatelite(self):
-        ...
+    def addSatelite(self, satelite_name: Satellite):
+        # Możemy tutaj dodać, więcej ograniczeń co do dodawania stacji. Jakiś kraj pochodzenia satelity czy coś innego
+        if satelite_name not in self.satelitesList:
+            self.satelitesList.append(satelite_name)
+        
+    def deleteSatelite(self, satelite_name: Satellite):
+        if satelite_name in self.satelitesList:
+            self.satelitesList.remove(satelite_name)
     
